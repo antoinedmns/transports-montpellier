9c6516ec4ca4ced5ba6c4c9729edff8da8d67c4f
antoinedmns <antoine.damiens1@orange.fr>
2024-04-09 13:04:20 +0200

    ajout itineraires

---
+++ statique/js/contenus/itineraires.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([43.6112422, 3.8767337,], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

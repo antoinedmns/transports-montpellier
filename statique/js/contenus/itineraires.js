2a4bf37ea010aec2c42cfa951e07605b10058525
antoinedmns <antoine.damiens1@orange.fr>
2024-04-17 19:22:54 +0200

    ajout itineraires

--- statique/js/contenus/itineraires.js
+++ statique/js/contenus/itineraires.js
@@ -5,25 +5,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-    function echangerTexte() {
-        // Ajouter la classe pour l'effet de rotation
-        const flecheConteneur = document.querySelector('.fleche-conteneur');
-        flecheConteneur.classList.add('spin-effect');
+function echangerTexte() {
+    // Ajouter la classe pour l'effet de rotation
+    const flecheConteneur = document.querySelector('.fleche-conteneur');
+    flecheConteneur.classList.add('spin-effect');
 
-        // Définir un délai avant d'échanger le texte
+    // Définir un délai avant d'échanger le texte
+    setTimeout(function () {
+        const boiteDebut = document.querySelector('.boite-debut');
+        const boiteFin = document.querySelector('.boite-fin');
+
+        // Échanger les valeurs
+        const temp = boiteDebut.value;
+        boiteDebut.value = boiteFin.value;
+        boiteFin.value = temp;
+
+        // Supprimer la classe après un certain délai pour permettre à l'animation de se terminer
         setTimeout(function () {
-            const boiteDebut = document.querySelector('.boite-debut');
-            const boiteFin = document.querySelector('.boite-fin');
+            flecheConteneur.classList.remove('spin-effect');
+        }, 250); // Correspond à la durée de l'animation 
 
-            // Échanger les valeurs
-            const temp = boiteDebut.value;
-            boiteDebut.value = boiteFin.value;
-            boiteFin.value = temp;
-
-            // Supprimer la classe après un certain délai pour permettre à l'animation de se terminer
-            setTimeout(function () {
-                flecheConteneur.classList.remove('spin-effect');
-            }, 250); // Correspond à la durée de l'animation 
-
-        }, 125); // Délai en millisecondes avant l'échange de texte 
-    }
+    }, 125); // Délai en millisecondes avant l'échange de texte 
+}

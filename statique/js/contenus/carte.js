ab582b1f0e455235a74e3d90d59e8d10c0608277
antoinedmns <antoine.damiens1@orange.fr>
2024-04-29 11:17:38 +0200

    ajout marker

--- statique/js/contenus/carte.js
+++ statique/js/contenus/carte.js
@@ -6,22 +6,62 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-var tramIcon = L.divIcon({className: 'fa-solid fa-train-tram fa-lg'});
-var busIcon = L.divIcon({className: 'fa-solid fa-bus-simple fa-lg'});
-var marker = L.marker([43.6112422, 3.8767337],{icon: busIcon}).addTo(map);
-marker.bindPopup('<div class="bandeau-popup"><p>Place de l\'Europe</p></div><span class="indicateur-ligne tramway ligne-1">T1</span>');
+//var tramIcon = L.divIcon({className: 'fa-solid fa-train-tram fa-lg'});
+//var busIcon = L.divIcon({className: 'fa-solid fa-bus-simple fa-lg'});
+//var marker = L.marker([43.6112422, 3.8767337],{icon: busIcon}).addTo(map);
+//marker.bindPopup('<div class="bandeau-popup"><p>Place de l\'Europe</p></div><span class="indicateur-ligne tramway ligne-1">T1</span>');
 
 
 (async () => {
 
+    const layerTram = L.layerGroup();
+    const layerBus = L.layerGroup();
     const resultatTraces = await coordinateur.api.getAPI('api/traces-reseau/');
-    if (resultatTraces === undefined) {ouvrirDialogue("erreurCoord")}
-    console.log(resultatTraces);
-    for(const ligne of resultatTraces) {
-        console.log(ligne)
-        for(const trajet of ligne.coordonnees) {
-            console.log(trajet)
-            L.polyline(trajet, { color: ligne.couleur}).addTo(map);
+
+    if (resultatTraces === undefined) { ouvrirDialogue("erreurCoord") };
+
+    for (let i = resultatTraces.length - 1; i >= 0; i--) {
+        const ligne = resultatTraces[i];
+        for (const trajet of ligne.coordonnees) {
+            if (ligne.num >= 5) {
+                layerBus.addLayer(L.polyline(trajet, { color: ligne.couleur, opacity: 0.5 }));
+            }
+            else {
+                layerTram.addLayer(L.polyline(trajet, { color: ligne.couleur, weight: 5 }));
+            }
         }
+    }
+
+    map.on('zoomend', function () {
+        console.log(map.getZoom())
+        if (map.getZoom() >= 14 && !map.hasLayer(layerBus)) {
+            map.addLayer(layerBus);
+        }
+        if (map.getZoom() < 14 && map.hasLayer(layerBus)) {
+            map.removeLayer(layerBus);
+        }
+    });
+
+    layerTram.addTo(map);
+    layerBus.addTo(map);
+
+})();
+
+(async () => {
+
+    const resultatArrets = await coordinateur.api.getAPI('/api/arrets/reseau');
+    var arretIcon = L.divIcon({ className: 'fa-solid fa-location-dot'});
+
+    if (resultatArrets == undefined) { ouvrirDialogue("erreurCoord") };
+
+    const coordArrets = Object.values(resultatArrets);
+
+    console.log(coordArrets);
+
+    for (const arrets of coordArrets) {
+        console.log(arrets.coords);
+        let coordsInverse = [arrets.coords[1], arrets.coords[0]];
+        let pointArret = L.marker(coordsInverse, { icon: arretIcon }).addTo(map);
+        pointArret.bindPopup('<div class="bandeau-popup"><p>'+arrets.nom+'</p></div></span>');
     }
 })();

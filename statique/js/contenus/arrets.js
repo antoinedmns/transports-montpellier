2b30573e74f902c67d6703c6e499793d2a2d47c0
xXTopinambourXx <clement.casado@icloud.com>
2024-05-03 15:12:29 +0200

    update arret.js + minor update arret.css

--- statique/js/contenus/arrets.js
+++ statique/js/contenus/arrets.js
@@ -9,9 +9,8 @@
     /* on récupère la clé (nom de l'arrêt) de chaque élément du dictionnaire */
     const nomArrets = Object.keys(resultatsArrets);
 
-    const arretsTram = ['1', '2', '3', '4'];
-    const arretsBusUrbain = ['6', '7', '8', '9', '10', '11', '14', '15', '16', '17', '18', '19', '51', '52', '53'];
-    // const arretsBusSuburbain = ['18', '20', '21', '22', '23', '24', '25', '26', '30', '32', '33', '34', '36', '38', '40', '41', '43', '44', '46'];
+    const arretsTram = infoLignes.tram;
+    const arretsBusUrbain = infoLignes.suburbain;
 
     nomArrets.forEach(arret => {
 
@@ -53,6 +52,7 @@
                 }
                 arretActuelLogo.appendChild(logoLigne);// J'ajoute chaque logo à la div contenant les logos
                 listeLigneArretActuel.push(ligne);
+
             }
         };
     
@@ -60,61 +60,54 @@
         arretActuelConteneur.appendChild(arretActuelNom);
         arretActuelConteneur.appendChild(arretActuelLogo);
         resultatRechercheConteneur.appendChild(arretActuelConteneur);
-    });
-    
 
-    /* on récupère tout les éléments avec la classe resultat-recherche (ce sont tous les arrêts) */
-    const arretsResultat  = document.querySelectorAll('.resultat-recherche');
+        const arretsResultatNom  = resultatsArrets[arret].nom;
+        console.log(arretsResultatNom);
 
-    const headerDialog = document.getElementById("title-header-dialog");
-    const bodyDialog = document.getElementById("body-dialog-box");
-    const titreDialogue = document.createElement("h2");
+        const headerDialog = document.getElementById("title-header-dialog");
+        const bodyDialog = document.getElementById("body-dialog-box");
+        const titreDialogue = document.createElement("h2");
 
-    /* on parcourt chacun des arrêts */
-    arretsResultat.forEach(arret => {
+        arretActuelConteneur.addEventListener('click', () =>{
 
-        /*quand on click sur l'arrêt choisi */
-        arret.addEventListener('click', () => {
-
-            // Je supprime le contenu précédent du body de la pop up afin d'éviter tout affichage de ligne de l'arret précédent
             bodyDialog.innerHTML = "";
 
-            console.log(arret.childNodes[1].childNodes[0].classList.contains("tramway"));
-            console.log(arret.classList);
+            const titreArretHeader = resultatsArrets[arret].nom;
+            console.log(titreArretHeader);
 
-            // Cela permet de récupérer le nom de l'arret pour par la suite l'ajouter dans le header de la pop-up
-            const titreArretHeader = arret.getElementsByClassName("resultat-nom-arret")[0].textContent;
+            const logosArretCopie = listeLigneArretActuel;
+            console.log(logosArretCopie);
 
-            const resultatsLogo = arret.childNodes[1].getElementsByClassName("resultat-logo");
-
-            // afin de parcourir toutes les logos de ligne de l'arrêt choisi je crée un array avec ces éléments
-            const resultatsLogoArray = Array.from(resultatsLogo);
-
-            // pour éviter les problèmes de réaffectation de logo j'initialise un clone des logos sur lequel je vais itérer ma boucle
-            const logosArretCopie = resultatsLogoArray.map(logo => logo.cloneNode(true));
-
-            logosArretCopie.forEach(ligne => {
+            listeLigneArretActuel.forEach(ligne => {
 
                 // numéro de ligne de transport
-                let numeroLigneActuelle = ligne.textContent;
+                let numeroLigneActuelle = ligne;
 
                 // En premier lieu on ajoute le conteneur d'élément
                 const conteneurLigne = document.createElement("div");
 
-                // Ici on test si la ligne actuel est une ligne de tram, de la forme : T1, T2, T3 ou T4.
-                if(numeroLigneActuelle.includes("T")){
-
-                    // si oui alors on supprime le T du numéro de la ligne (pour éviter d'avoir des soucis par la suite)
-                    numeroLigneActuelle = numeroLigneActuelle.replace('T', "");
-
-                }
-
                 // on ajoute l'attribut qui lie la couleur au background du conteneur avec les éléments de la ligne
                 conteneurLigne.setAttribute("class",`dialog-ligne ligne-${numeroLigneActuelle}`);
 
+
+                // Ici je recréé le logo pour chaque ligne de l'arrêt
+                const logoLigne = document.createElement("span");
+                if(arretsTram.includes(ligne)){
+                    logoLigne.setAttribute("class", `resultat-logo indicateur-ligne tramway ligne-${ligne}`);
+                    logoLigne.innerHTML = "T"+ligne;
+                }
+                else if(arretsBusUrbain.includes(ligne)){
+                    logoLigne.setAttribute("class", `resultat-logo indicateur-ligne busMTP ligne-${ligne}`);
+                    logoLigne.innerHTML = ligne;
+                }
+                else {
+                    logoLigne.setAttribute("class", `resultat-logo indicateur-ligne bus3M ligne-${ligne}`);
+                    logoLigne.innerHTML = ligne;
+                }
+
                 const logoActuel = document.createElement("h1");
                 logoActuel.setAttribute("class", "logo-ligne");
-                logoActuel.appendChild(ligne);
+                logoActuel.appendChild(logoLigne);
                 console.log("logo actuel :", logoActuel);
 
                 // Ensuite on ajoute le logo de la ligne au conteneur d'élément
@@ -221,7 +214,7 @@
                 conteneurFleche.addEventListener('click', () => {
 
                     // TEMPORAIRE PCK LE CODE EST A RETRAVAILLER
-                    const idArret = resultatsArrets[arret.getElementsByClassName("resultat-nom-arret")[0].textContent].id;
+                    const idArret = resultatsArrets[arret].id;
 
                     // fermer la boite de dialogue et charger la page
                     console.log(numeroLigneActuelle, idArret);
@@ -234,16 +227,18 @@
 
             titreDialogue.setAttribute("class", "second-title");
 
-            /* et à l'intérieur j'écris le nom de l'ârret*/
+            // et à l'intérieur j'écris le nom de l'ârret
             titreDialogue.innerHTML = titreArretHeader;
 
-            /* et je l'ajoute enfin à la page EJS */
+            // et je l'ajoute enfin à la page EJS
             headerDialog.appendChild(titreDialogue);
 
+            // j'ouvre la pop-up
             ouvrirDialogue("ligneInfo");
 
-        });
-
+            
+        })
+        
     });
 
     const barreRechercheArret = document.getElementById('recherche');
@@ -255,8 +250,10 @@
 
         /* Barre de recherche active (première lettre relâché) */
         if(!barreRechercheArret.classList.contains("recherche-active") && barreRechercheArret.value.length > 0) {
+
             barreRechercheArret.classList.remove("recherche-inactive");
             barreRechercheArret.classList.add("recherche-active");
+
         }
         
         /* Barre de recherche inactive (plus de lettre dans la barre de recherche) */
